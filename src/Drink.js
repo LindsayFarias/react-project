0d4d1759ac9c6d7d5eed5f75138ef860463d32b4
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-05 11:48:50 -0400

    Added ability to get random drink or by name

--- src/Drink.js
+++ src/Drink.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Ingredients from "./Ingredients";
 
 class Drink extends React.Component {
     constructor({ name, ingredients, instructions, image }) {
         super()
         this.state = {
+            searchQuery: "",
             name: name,
             ingredients: ingredients,
             instructions: instructions,
@@ -12,11 +14,18 @@ class Drink extends React.Component {
     }
 
     async componentDidMount() {
-        this.getRandomDrink()
+        // this.setState({})
+        this.getDrink()
     }
 
-    async getRandomDrink() {
-        let result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    async getDrink(drinkName) {
+        let result = {}
+        if (!drinkName) {
+            result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        } else {
+            result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName)
+        }
+
         console.log("Result", result)
         let json = await result.json()
         console.log("JSON", json.drinks[0].strDrink)
@@ -24,9 +33,20 @@ class Drink extends React.Component {
         let drink = json.drinks[0];
 
         let name = drink.strDrink
-        let ingredients = {}
+        let ingredients = []
         let instructions = drink.strInstructions
         let image = drink.strDrinkThumb
+
+        for (let i = 1; i < 15; i++) {
+            let ingredientName = "strIngredient" + i
+            let measurementName = "strMeasure" + i
+
+            if (drink[ingredientName]) {
+                ingredients.push({ ingredient: drink[ingredientName], measurement: drink[measurementName] })
+            }
+        }
+
+        console.log(ingredients)
 
         this.setState({ name: name, ingredients: ingredients, instructions: instructions, image: image })
     }
@@ -36,6 +56,7 @@ class Drink extends React.Component {
         return (
             <div className="drink">
                 <div>Drink Name: {this.state.name}</div>
+                <Ingredients ingredients={this.state.ingredients} />
                 <p>Instructions: {this.state.instructions}</p>
                 <div><img src={this.state.image}></img></div>
             </div>
