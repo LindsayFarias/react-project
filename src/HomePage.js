fbf48d68cc8b4d781ae4be12b1087be2c0ffc18f
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-05 17:23:49 -0400

    Added formatting to home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,27 @@
 import React from "react"
+import { Card, Container, Image } from "react-bootstrap";
 
 const HomePage = () => {
     return (
-        <div className="home-page">
-            <h1>Are you ready to find the best drink ever?</h1>
-            <p>Click Find Random Drink to be surprised.</p>
-            <p>Enter a drink by name and search how to make it.</p>
-        </div>
+        <Container className="mx-auto my-5" >
+            <Card className="drink-card">
+                <Card.Body>
+                    <Card.Text>
+                        <h1>Are you ready to find the best drink ever?</h1>
+                        <p>Click the "Find Random Drink" Button to be surprised.</p>
+                        <p>Enter a drink by name and search how to make it.</p>
+                    </Card.Text>
+                    <Image className="shadow-lg p-3 mb-5 bg-light rounded" src="https://c.tenor.com/zFso1Q9MpYkAAAAC/dog-cocktail.gif" />
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
 
 export default HomePage
+// <div className="home-page">
+//     <h1>Are you ready to find the best drink ever?</h1>
+//     <p>Click Find Random Drink to be surprised.</p>
+//     <p>Enter a drink by name and search how to make it.</p>
+//     <img src="https://c.tenor.com/zFso1Q9MpYkAAAAC/dog-cocktail.gif" />
+// </div>
