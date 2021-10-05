8bd1653c6559adcd5af41af6da37fefd665f85a3
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-05 14:33:22 -0400

    Moved drink logic to App class

--- src/App.js
+++ src/App.js
@@ -9,18 +9,69 @@ import NavBar from './NavBar';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {}
+
+    this.search = this.search.bind(this);
+    this.getDrink = this.getDrink.bind(this);
+
+    this.state = {
+      name: "",
+      ingredients: [],
+      instructions: "",
+      image: ""
+    }
   }
 
+  async componentDidMount() {
+    this.getDrink()
+  }
+
+  search() {
+    let drinkSearch = document.getElementById("drinkSearch")
+    console.log("Searching...", drinkSearch.value)
+    this.getDrink(drinkSearch.value)
+  }
+
+  async getDrink(drinkName) {
+    let result = {}
+    if (!drinkName) {
+      result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    } else {
+      result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName)
+    }
+
+    console.log("Result", result)
+    let json = await result.json()
+    console.log("JSON", json.drinks[0].strDrink)
+
+    let drink = json.drinks[0];
+
+    let name = drink.strDrink
+    let ingredients = []
+    let instructions = drink.strInstructions
+    let image = drink.strDrinkThumb
+
+    for (let i = 1; i < 15; i++) {
+      let ingredientName = "strIngredient" + i
+      let measurementName = "strMeasure" + i
+
+      if (drink[ingredientName]) {
+        ingredients.push({ ingredient: drink[ingredientName], measurement: drink[measurementName] })
+      }
+    }
+
+    console.log(ingredients)
+
+    this.setState({ name: name, ingredients: ingredients, instructions: instructions, image: image })
+  }
 
   render() {
     return (
       <div className="App">
         <header>
-          <NavBar drink={this.setState.drink} />
+          <NavBar searchFunc={this.search} randomDrinkFunc={this.getDrink} />
         </header>
         <main className="main-body">
-          <Drink />
+          <Drink name={this.state.name} ingredients={this.state.ingredients} instructions={this.state.instructions} image={this.state.image} />
         </main>
       </div>
     );
@@ -35,11 +86,11 @@ export default App;
 // Use fetch to make your API call - DONE
 // Use componentDidMount and setState properly in conjunction - DONE
 // At least 1 classical component (best practice - at the top level) - DONE
-// At least 3 functional components - 1/3
-// Some CSS styles for components
-// At least 2 event handlers
-// At least one form/field (somewhere a user types things or enters data)
-// This data must be used somehow - to set state, to make an API call, etc.
-// Components that nest to a depth of at least three components:
+// At least 3 functional components - DONE
+// Some CSS styles for components (bootstrap....?)
+// At least 2 event handlers - DONE
+// At least one form/field (somewhere a user types things or enters data) - DONE
+// This data must be used somehow - to set state, to make an API call, etc. - DONE
+// Components that nest to a depth of at least three components: DONE
 // Example: App > List > ListItem
 // Passing a method that can affect the state of the top level component as props down to the lowest level component (e.g. in the example above, the ListItem would need to be able to use a method to change the state of App
--- src/Drink.js
+++ src/Drink.js
@@ -1,67 +1,15 @@
 import React from "react";
 import Ingredients from "./Ingredients";
 
-class Drink extends React.Component {
-    constructor({ name, ingredients, instructions, image }) {
-        super()
-        this.state = {
-            searchQuery: "",
-            name: name,
-            ingredients: ingredients,
-            instructions: instructions,
-            image: image
-        }
-    }
-
-    async componentDidMount() {
-        // this.setState({})
-        this.getDrink()
-    }
-
-    async getDrink(drinkName) {
-        let result = {}
-        if (!drinkName) {
-            result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-        } else {
-            result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName)
-        }
-
-        console.log("Result", result)
-        let json = await result.json()
-        console.log("JSON", json.drinks[0].strDrink)
-
-        let drink = json.drinks[0];
-
-        let name = drink.strDrink
-        let ingredients = []
-        let instructions = drink.strInstructions
-        let image = drink.strDrinkThumb
-
-        for (let i = 1; i < 15; i++) {
-            let ingredientName = "strIngredient" + i
-            let measurementName = "strMeasure" + i
-
-            if (drink[ingredientName]) {
-                ingredients.push({ ingredient: drink[ingredientName], measurement: drink[measurementName] })
-            }
-        }
-
-        console.log(ingredients)
-
-        this.setState({ name: name, ingredients: ingredients, instructions: instructions, image: image })
-    }
-
-
-    render() {
-        return (
-            <div className="drink">
-                <div>Drink Name: {this.state.name}</div>
-                <Ingredients ingredients={this.state.ingredients} />
-                <p>Instructions: {this.state.instructions}</p>
-                <div><img src={this.state.image}></img></div>
-            </div>
-        )
-    }
+const Drink = ({ name, ingredients, instructions, image }) => {
+    return (
+        <div className="drink">
+            <div>Drink Name: {name}</div>
+            <Ingredients ingredients={ingredients} />
+            <p>Instructions: {instructions}</p>
+            <div><img src={image}></img></div>
+        </div>
+    )
 }
 
 export default Drink
