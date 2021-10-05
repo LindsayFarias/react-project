9c3c73dacfa313189677effa2a401a3585335228
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-05 14:40:29 -0400

    Fixed formatting of NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,35 +1,37 @@
 import React from "react"
-import Drink from "./Drink"
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap'
 
-const NavBar = (props) => (
-    <Navbar bg="dark" expand="lg" variant="dark">
-        <Navbar.Brand href="#">SDI 7's Drink Finder</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-            <Nav
-                className="mr-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-            >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Button variant="outline-success">Find Random Drink</Button>
+const NavBar = (props) => {
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#">SDI 7's Drink Finder</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Button onClick={() => { props.randomDrinkFunc() }} variant="outline-success">Find Random Drink</Button>
 
-                <Nav.Link href="#" disabled>
-                    Link
-                </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-                <FormControl
-                    type="search"
-                    placeholder="Enter Drink type"
-                    className="mr-2"
-                    aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-            </Form>
-        </Navbar.Collapse>
-    </Navbar>
-)
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search by Drink Name"
+                            className="me-2"
+                            aria-label="Search"
+                            id="drinkSearch"
+                            ref={props.searchRef}
+                        />
+                        <Button onClick={() => { props.searchFunc() }} variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
 
 export default NavBar
